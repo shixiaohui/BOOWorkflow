e202dc69a9103cdb568094e63fdd29bc2999c4eb
rinkako <jiajidi@163.com>
2017-03-06 12:36:30 +0800

    commit test objs

--- BOODemo/BOODemo/Form1.cs
+++ BOODemo/BOODemo/Form1.cs
@@ -8,8 +8,9 @@ using System.Text;
 using System.Windows.Forms;
 
 using com.sysu.workflow;
-using com.sysu.workflow.model;
 using com.sysu.workflow.io;
+using com.sysu.workflow.env;
+using com.sysu.workflow.model;
 using com.sysu.workflow.env.jexl;
 
 
@@ -18,16 +19,22 @@ namespace BOODemo
     extern alias OpenJDKCore;
     public partial class Form1 : Form
     {
+
+        SCXMLExecutor executor;
+
         public Form1()
         {
             InitializeComponent();
 
             OpenJDKCore.java.net.URL url = new OpenJDKCore.java.net.URL("file", "", "helloworld.xml");
             SCXML scxml = SCXMLReader.read(url);
-            SCXMLExecutor executor = new SCXMLExecutor();
-            executor.setStateMachine(scxml);
             Evaluator ev = new JexlEvaluator();
-            executor.setEvaluator(ev);
+            executor = new SCXMLExecutor(ev, new MulitStateMachineDispatcher(), new SimpleErrorReporter());
+            executor.setStateMachine(scxml);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             executor.go();
         }
     }
