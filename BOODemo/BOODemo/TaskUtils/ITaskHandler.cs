17e0ca0daa66422429494e2ecbcd098199bc752e
rinkako <jiajidi@163.com>
2017-03-09 02:58:45 +0800

    TaskFactory, ITaskHandler, etc.

---
+++ BOODemo/BOODemo/TaskUtils/ITaskHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace BOODemo.TaskUtils
+{
+    /// <summary>
+    /// 任务处理器接口类：为任务处理器提供公共的方法接口
+    /// 所有的任务处理器的实现都要实现该接口
+    /// </summary>
+    internal interface ITaskHandler
+    {
+        /// <summary>
+        /// 初始化任务处理器
+        /// </summary>
+        /// <param name="paraDict">参数字典，键是形参，键值是实参对象</param>
+        /// <returns>初始化任务是否成功</returns>
+        bool Init(Dictionary<string, object> paraDict);
+
+        /// <summary>
+        /// 开始处理任务
+        /// </summary>
+        /// <returns>任务是否成功结束</returns>
+        bool Begin();
+
+        /// <summary>
+        /// 强制结束任务
+        /// </summary>
+        /// <returns>是否已经成功强制结束了任务</returns>
+        bool Terminate();
+
+        /// <summary>
+        /// 查询任务是否已经完成
+        /// </summary>
+        /// <returns>任务是否已经完成</returns>
+        bool IsFinished();
+
+        /// <summary>
+        /// 获取任务处理的返回结果
+        /// </summary>
+        /// <param name="result">[out] 返回结果的包装</param>
+        /// <returns>是否成功获取到了要返回的执行结果</returns>
+        bool GetResult(out object result);
+    }
+}
---
+++ BOODemo/BOODemo/TaskUtils/TaskFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace BOODemo.TaskUtils
+{
+    /// <summary>
+    /// 任务工厂类：为应用程序提供产生任务相应的处理器的方法
+    /// </summary>
+    internal static class TaskFactory
+    {
+        /// <summary>
+        /// 通过任务名字获取任务处理器对象
+        /// </summary>
+        /// <param name="taskName">任务的名字</param>
+        /// <returns>该任务名字所对应的处理器对象</returns>
+        public static ITaskHandler GetTaskHandlerByTaskName(string taskName)
+        {
+            var findHandlerName = String.Format("{0}Handler", taskName);
+            var findType = TaskFactory.TypeVector.Find((x) => String.Compare(x.Name, findHandlerName, true) == 0);
+            if (findType != null)
+            {
+                return (ITaskHandler)Activator.CreateInstance(findType);
+            }
+            else
+            {
+                throw new TaskHandlerNotExistException(taskName);
+            }
+        }
+
+        /// <summary>
+        /// 静态构造函数
+        /// </summary>
+        static TaskFactory()
+        {
+            var TypeArr = Assembly.GetExecutingAssembly().GetTypes();
+            var HandlerTypeArr = from h in TypeArr
+                                 where h.FullName.StartsWith("BOODemo.TaskWarehouse.", StringComparison.CurrentCultureIgnoreCase)
+                                 select h;
+            TaskFactory.TypeVector = new List<Type>(HandlerTypeArr);
+        }
+
+        /// <summary>
+        /// 任务处理器类型向量
+        /// </summary>
+        private static List<Type> TypeVector;
+
+        /// <summary>
+        /// 任务处理器不存在错误
+        /// </summary>
+        private class TaskHandlerNotExistException : Exception
+        {
+            /// <summary>
+            /// 构造函数
+            /// </summary>
+            /// <param name="taskName">被调用任务名</param>
+            public TaskHandlerNotExistException(string taskName)
+                : base(String.Format("BOOWorkflow called task: [{0}], but its handler does not exist.", taskName))
+            { }
+        }
+    }
+}
